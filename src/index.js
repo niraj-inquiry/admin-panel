912c87dc7194b1cbf1c1da6424fb648456e493cb
niraj-inquiry <niraj.inquiry@gmail.com>
2023-06-24 15:12:13 +0530

    few changes in package.json

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import store from './store'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </Provider>,
+  
 )
 
 // If you want to start measuring performance in your app, pass a function
